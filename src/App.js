8cdf2c118652523101c511623d557b929ea9e87e
Vova <tarabanvv@mail.ru>
2024-02-09 20:27:41 +0800

    Настроить проект

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { styled } from 'styled-components';
+
+const Div = styled.div`
+	text-align: center;
+`;
 
 export const App = () => {
 	return (
-		<div className='App'>
-			<header className='App-header'>
-				<img src={logo} className='App-logo' alt='logo' />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className='App-link'
-					href='https://reactjs.org'
-					target='_blank'
-					rel='noopener noreferrer'
-				>
-					Learn React!
-				</a>
-			</header>
-		</div>
+		<>
+			<Div>
+				<div>456</div>
+				<i className='fa fa-camera-retro'></i>
+			</Div>
+		</>
 	);
 };
